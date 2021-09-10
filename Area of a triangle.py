ee34c31a0100d5fc608b30b7d92179ca1a4e1f11
fabioT99 <turcofabio99@gmail.com>
2021-09-10 18:59:43 +0100

    Sum

--- Area of a triangle.py
+++ Area of a triangle.py
@@ -1,4 +1,4 @@
-lenght = (input("lenght value : "))
-width =  (input("width value : "))
+lenght = float(input("lenght value : "))
+width =  float(input("width value : "))
 area = width * lenght / 2
-print ("The area of the triangle is : " %area)
+print ("The area of the triangle is %.2f " %area)
