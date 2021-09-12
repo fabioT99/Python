2f624333e7da73c982745e21971e55c934b16858
fabioT99 <turcofabio99@gmail.com>
2021-09-12 18:23:28 +0100

    Print a message if a variable is less than 10 and a different message if the variable is greater than or equal to 10

---
+++ Print a message if a variable is less than 10 and a different message if the variable is greater than or equal to 10.py
@@ -0,0 +1,11 @@
+firstNumber = 5
+secondNumber = 11
+if firstNumber < 10:
+    print("The variable is less than 10")
+else:
+    print("The variable is greater than 10")
+        
+if secondNumber <= 10:
+    print("The variable is less than 10")
+else:
+    print("The variable is greater than or equal to 10")
