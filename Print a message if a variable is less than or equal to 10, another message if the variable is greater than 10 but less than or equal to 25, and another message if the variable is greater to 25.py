39baa95057b8ca95639682a3af6a971a13196b04
fabioT99 <turcofabio99@gmail.com>
2021-09-12 19:01:17 +0100

    Print a message if a variable is less than or equal to 10, another message if the variable is greater than 10 but less than or equal to 25, and another message if the variable is greater to 25

---
+++ Print a message if a variable is less than or equal to 10, another message if the variable is greater than 10 but less than or equal to 25, and another message if the variable is greater to 25.py
@@ -0,0 +1,23 @@
+firstNumber = 10
+secondNumber = 25
+thirdNumber = 30
+if firstNumber <= 10:
+    print("The variable is less than or equal to 10")
+elif firstNumber < 10:
+    print("The variable is less to 10")
+else:
+    print("The variable is greater than 10")
+
+if secondNumber < 10:
+    print("The variable is less to 10")
+elif secondNumber >= 25:
+    print("The variable is greater than 10 but less than or equal to 25")
+else:
+    print("There is an error")
+
+if thirdNumber < 25:
+    print("The variable is less to 25")
+elif thirdNumber == 25:
+    print("The variable is equal to 25")
+else:
+    print("The variable is greater to 25")
