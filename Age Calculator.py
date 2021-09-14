83dca37151accc8566f416aeea96410b513e40e4
fabioT99 <turcofabio99@gmail.com>
2021-09-14 19:37:17 +0100

    Age Calculator

--- Age Calculator.py
+++ Age Calculator.py
@@ -15,9 +15,9 @@ def age_calculator(name, current_year, DOB):
     print("age: ")
     print(age)
     if age < 18:
-        return name + " is not an adult"
+        return name + " is not an adult "
     else:
-        return name + " is an adult"
+        return name + " is an adult "
 result1 = age_calculator(name1, current_year1, DOB1)
 result2 = age_calculator(name2, current_year2, DOB2)
 result3 = age_calculator(name3, current_year3, DOB3)
